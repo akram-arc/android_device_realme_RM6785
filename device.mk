545486ff42deb3f1f6e7d2680a585a346e4d07b9
Matsvei Niaverau <matvej2002@gmail.com>
2026-06-07 19:26:38 +0200

    RM6785: Set flag for custom usb init
    Change-Id: I62c03c634faf2b80a2425c41c0db825512ba86e8
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -346,6 +346,8 @@ PRODUCT_SOONG_NAMESPACES += \
 AB_OTA_UPDATER := false
 
 
+$(call soong_config_set_bool,mediatek_gadget,use_custom_usb_gadget_rc,true)
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator-service.mediatek
